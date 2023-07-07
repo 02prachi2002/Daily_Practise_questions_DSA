caebed42844592b21d226ad02a692e9e7610df84
02prachi2002 <211288@juitsolan.in>
2023-07-07 11:34:36 +0530

    first commit

---
+++ pattern13.cpp
@@ -0,0 +1,46 @@
+//hollow daimond
+#include<iostream>
+using namespace std;
+int main()
+
+{
+    int num;
+   
+    cout<< "Enter the num of pattern you want:";
+    cin>>num;
+    for(int i=0;i<num;i++)
+    {
+        int k=0;
+        for(int j=0;j<((2*num)-1);j++)
+        {
+            if(j<num-i-1){
+                cout<<" ";
+            }
+            else if(k<2*i+1)
+            {
+                if(k==0||k==2*i||i==num-1)
+                {
+                     cout<<"*";
+                }
+               
+                else{
+                    cout<<" ";
+                }
+                k++;
+            }
+
+            
+            else{
+                cout<<" ";
+            }
+        }
+        cout<<"\n";
+    }
+
+for(int i=0;i<num;i++){
+    cout<<" ";
+}
+
+for(int j=0;j<2*num-2*i-1;)
+    return 0;
+}
